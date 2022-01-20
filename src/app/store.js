c9746f703ebc447619ad7048394dd72d8066490d
satoshi3451 <wwwesk@gmail.com>
2022-01-20 12:27:45 +0900

    first commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import loginReducer from "../features/login/loginSlice";
+import taskReducer from  "../features/task/taskSlice";
 
 export const store = configureStore({
   reducer: {
+    task: taskReducer,
     counter: counterReducer,
+    login: loginReducer,
   },
 });
